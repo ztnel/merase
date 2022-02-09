28135177647c91d52381d08941519260a75d9c3d
cSDes1gn <christian911@sympatico.ca>
2022-02-08 21:38:17 -0500

    merase example

---
+++ examples/README.md
@@ -0,0 +1,17 @@
+# Merase Example
+
+Modified: 2022-02
+
+## Build
+Compile the example and link against `merase-static`:
+```bash
+mkdir build
+cd build
+cmake -DLOG_LEVEL=0 ..
+make -j
+```
+
+Run the example:
+```bash
+./merase-ex
+```
---
+++ examples/main.c
@@ -0,0 +1,25 @@
+/**
+ * @file main.c
+ * @author Christian Sargusingh (christian@leapsystems.online)
+ * @brief Example for merase usage
+ * @version 0.1
+ * @date 2022-02
+ * 
+ * @copyright Copyright © 2022 Christian Sargusingh
+ * 
+ */
+#include <merase.h>
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL 0
+#endif
+
+int main(int argc, char **argv) {
+  // load in log level from a build preset
+  logger_set_level(LOG_LEVEL);
+  critical("Merase logging level set to %d", LOG_LEVEL);
+  error("Hello from Merase: A C logger for micro-embedded systems.");
+  warning("Merase is derived from polymerase, an enzyme for building DNA/RNA sequences");
+  info("Contributions are welcome.");
+  trace("Please contact me at christian@leapsystems.online if you have any questions.");
+}
